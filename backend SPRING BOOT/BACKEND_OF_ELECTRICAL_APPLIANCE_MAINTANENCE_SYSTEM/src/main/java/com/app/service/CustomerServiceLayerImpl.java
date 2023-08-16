5ae68fff70dbfc800499136b477c61837ae708ee
omkarladofficial <omkarlad.official@google.com>
2023-08-17 02:41:34 +0530

    read_of_customer

--- backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/service/CustomerServiceLayerImpl.java
+++ backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/service/CustomerServiceLayerImpl.java
@@ -1,12 +1,28 @@
 package com.app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.app.entity.Customer;
 import com.app.repository.CustomerRepositoryIF;
 
 public class CustomerServiceLayerImpl implements CustomerServiceLayerIF {
-	@Autowired 
+
+	@Autowired
 	private CustomerRepositoryIF custRepo;
 	
+	@Autowired
+	private ModelMapper modelMapper;
 
+	@Override
+	public Customer getCustomerDetails(Long customerId) {
+		
+		return custRepo.findById(customerId).orElseThrow(() -> new RuntimeException("Invalid emp id !!!!!"));
+	}
+
+//	
 }
