eb073d670b921fc5fb925973a737b89baf1e8d06
Avanish Page <avanishpp@gmail.com>
2023-08-17 01:14:59 +0530

    added_customerdto

--- backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/controller/CustomerController.java
+++ backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/controller/CustomerController.java
@@ -2,19 +2,18 @@ package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.service.CustomerServiceLayerIF;
 
 @RestController
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerServiceLayerIF serviceLayer;
-	
-	@PostMapping
-	public void createCustomer() {
-		
-	}
+
+	// @PostMapping
+	// public void createCustomer(@RequestBody ) {
 
 }
