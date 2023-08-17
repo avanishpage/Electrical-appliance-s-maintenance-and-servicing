c5e54b695f80fe808d19802346604ac183b94125
“himanshumali007” <himanshu.mali911@gmail.com>
2023-08-17 21:08:07 +0530

    Vendor Stuff

---
+++ backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/repository/VendorRepositoryIF.java
@@ -0,0 +1,10 @@
+package com.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entity.Vendor;
+
+
+	public interface VendorRepositoryIF extends JpaRepository<Vendor, Long> {
+
+	}
