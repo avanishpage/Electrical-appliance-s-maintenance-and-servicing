e6f61bdee12f738abcad67b58fd4f8e2a88550fa
Avanish Page <avanishpp@gmail.com>
2023-08-22 19:20:37 +0530

    constructor with no id added in personDto

--- backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/dto/PersonDto.java
+++ backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/dto/PersonDto.java
@@ -1,3 +1,4 @@
+
 package com.app.dto;
 
 
@@ -38,6 +39,21 @@ public class PersonDto {
 	
 	@NotBlank(message = "phone number cannot be blank!!")
 	private String phoneNumber;
+
+	public PersonDto(@NotBlank(message = "first name cannot be blank!!") String firstName,
+			@NotBlank(message = "last name cannot be blank!!") String lastName,
+			@NotBlank(message = "email is mandatory!!") @Email String email,
+			@NotBlank(message = "password is mandatory!!") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,12}$", message = "password must be min 4 and max 12 length containing atleast 1 uppercase, 1 lowercase, 1 special character and 1 digit ") String password,
+			@NotBlank(message = "address cannot be blank!!") String address,
+			@NotBlank(message = "phone number cannot be blank!!") String phoneNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
 	
 	
 	
