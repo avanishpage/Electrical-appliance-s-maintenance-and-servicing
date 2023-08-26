a12ccadf3eebbcc950f2089dbf958eff345038d1
Shantanu <shantanusawant35@gmail.com>
2023-08-27 03:15:49 +0530

    added vendor functionality

--- frontend ReactJs/FRONTEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SERVICES_SYSTEM/src/App.js
+++ frontend ReactJs/FRONTEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SERVICES_SYSTEM/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -11,9 +11,19 @@ import AboutUsPage from './components/AboutUsPage';
 import AboutImage from './components/AddImage';
 import ContactUsPage from './components/ContactUsPage';
 import AddImage from './components/AddImage';
+import Sidebar from './components/Sidebar';
+import GetOrders from './VenderPages/GetOrders';
+import VenderDetails from './VenderPages/VenderDetails';
+import UpdateServices from './VenderPages/UpdateServices';
+import UpdateVenderDetails from './VenderPages/UpdateVenderDetails';
+import VendorServices from './VenderPages/VendorServices';
+import UpdateOrderStatus from './VenderPages/UpdateOrderStatus';
+import AddService from './VenderPages/AddService';
+import Logout from './VenderPages/Logout';
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-        
+        <div>
         <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/AboutUsPage" element={<AboutUsPage />} />
@@ -22,11 +32,24 @@ function App() {
         <Route path="/RegistrationPageVendor" element={<RegistrationPageVendor />} />
         <Route path="/RegistrationPageVendor/AddImage" element={<AddImage />} />
         <Route path="/SignInPage" element={<SignInPage />} />
-        <Route path="/SignInPageVendor" element={<SignInPageVendor />} />
+        <Route path="/SignInPageVendor" element={<SignInPageVendor setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/ForgotPasswordPage" element={<ForgotPasswordPage />} />
         </Routes>
-        
-   
+        {isLoggedIn && ( 
+          <Sidebar>
+            <Routes>
+              <Route path="/getorders" element={<GetOrders />} />
+              <Route path="/vendor/:id" element={<VenderDetails />} />
+              <Route path="/update/:id" element={<UpdateServices />} />
+              <Route path="/addService" element={<AddService />} />
+              <Route path="/updateOrderStatus" element={<UpdateOrderStatus />} />
+              <Route path="/updatevendordetails" element={<UpdateVenderDetails />} />
+              <Route path="/serviceDetails" element={<VendorServices />} />
+              <Route path="/logout" element={<Logout setIsLoggedIn={setIsLoggedIn}/>} />
+            </Routes>
+          </Sidebar>
+        )}
+   </div>
   );
 }
 
--- frontend ReactJs/FRONTEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SERVICES_SYSTEM/src/components/SignInPageVendor.js
+++ frontend ReactJs/FRONTEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SERVICES_SYSTEM/src/components/SignInPageVendor.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './css/SignInPage.css'; 
-
-function SignInPageVendor() {
+import axios from 'axios';
+function SignInPageVendor({setIsLoggedIn}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const navigate = useNavigate();
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -14,11 +14,35 @@ function SignInPageVendor() {
     setPassword(e.target.value);
   };
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+    
+  //   console.log('Sign in:', email, password);
+  // };
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    console.log('Sign in:', email, password);
+
+    // Send a request to the backend to check if the user exists
+    axios.post('http://localhost:8080/vendor/login', {
+      email:email,
+      password:password,
+    })
+    .then(response => {
+      if (!response.data.exists) {
+        // User exists, navigate to UserDetailsPage
+        localStorage.setItem('vendor',JSON.stringify(response.data))
+        setIsLoggedIn(true);
+        navigate('/getorders')
+      } else {
+        // User doesn't exist, display an error or handle accordingly
+        console.log('User does not exist',{ email, password });
+      }
+    })
+    .catch(error => {
+      console.error('Error checking user:', error);
+    });
   };
+
 
   return (
     <div className="sign-in-page">
