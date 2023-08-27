bb56dafd22eaf6ee4c8ddc9bb46130935330fb74
Avanish Page <avanishpp@gmail.com>
2023-08-28 01:15:51 +0530

    working code without security

--- backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/BackendOfElectricalApplianceMaintanenceSystemApplication.java
+++ backend SPRING BOOT/BACKEND_OF_ELECTRICAL_APPLIANCE_MAINTANENCE_SYSTEM/src/main/java/com/app/BackendOfElectricalApplianceMaintanenceSystemApplication.java
@@ -5,8 +5,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class BackendOfElectricalApplianceMaintanenceSystemApplication {
@@ -22,10 +21,10 @@ public class BackendOfElectricalApplianceMaintanenceSystemApplication {
 		return mapper;
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 	
 
